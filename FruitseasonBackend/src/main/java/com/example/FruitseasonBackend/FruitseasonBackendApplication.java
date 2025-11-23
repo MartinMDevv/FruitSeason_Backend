2f36476a2d6c5193516014abc0370c7a7e456ee9
MartinDev <marinmalvarez@gmail.com>
2025-11-23 03:35:01 -0300

    First commit

---
+++ FruitseasonBackend/src/main/java/com/example/FruitseasonBackend/FruitseasonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.FruitseasonBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitseasonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitseasonBackendApplication.class, args);
+	}
+
+}
---
+++ FruitseasonBackend/src/test/java/com/example/FruitseasonBackend/FruitseasonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FruitseasonBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitseasonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
